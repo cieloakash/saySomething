494ed708740e4493a49d2e2b26e5ce42a9eae2b3
cieloakash <akpakash490@gmail.com>
2024-01-16 21:15:23 +0530

    file reschedule

---
+++ fronted/src/components/Register.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { FaUser } from "react-icons/fa";
+
+function Register() {
+  return (
+    <div>
+        <div>
+            <span className=' '>
+                <FaUser  className=' text-6xl rounded-full bg-gray-400'/>
+            </span>
+        </div>
+    </div>
+  )
+}
+
+export default Register
